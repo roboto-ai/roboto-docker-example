d45475894ae119ae52f6b1a6658ce6db59348809
Yves <yves.albers@gmail.com>
2025-09-04 18:17:01 +0200

    add device id example

--- create_dataset.py
+++ create_dataset.py
@@ -2,19 +2,20 @@ from roboto import Dataset
 from pathlib import Path
 
 input_file = Path("test.ulg")
-ds = Dataset.create(name="my_dataset_123")
+device_id = "vehicle_001"
+
+ds = Dataset.create(name="my_dataset_123", device_id=device_id, create_device_if_missing=True)
 
 print(f"Created dataset: {ds.dataset_id}")
 
 # Upload the .ulg file, retaining filename as destination
-ds.upload_file(input_file, file_destination_path=input_file.name)
+ds.upload_file(input_file, file_destination_path=input_file.name, device_id=device_id)
 
 # Add some metadata
 ds.put_metadata({
-    "vehicle_id": "vehicle_001",
     "test_type": "night_flight",
-    "weather.condition": "rainy",
-    "weather.temperature": 20
+    "weather": "rainy",
+    "temperature": 20
 })
 
 # Add some tags
